e6b9005fa9f9d5b60a2aa7fed3580560ba0b251a
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-10-06 20:36:00 -0400

    Initial Commit

---
+++ instagram_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram_clone
+//
+//  Created by Emin Mammadzada on 10/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
