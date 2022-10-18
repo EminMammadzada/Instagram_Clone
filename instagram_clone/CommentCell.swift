f77b706cb1fee760decdcc9ff96548045b6db74b
Emin Mammadzada <eminmamedzade37@gmail.com>
2022-10-18 11:10:15 -0400

    display comments

---
+++ instagram_clone/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  instagram_clone
+//
+//  Created by Emin Mammadzada on 10/18/22.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- instagram_clone/FeedViewController.swift
+++ instagram_clone/FeedViewController.swift
@@ -26,7 +26,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         
         let query = PFQuery(className: "Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
@@ -38,24 +38,44 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PosterViewCell") as! PosterViewCell
-        let post = self.postsArray[indexPath.row]
         
-        let user = post["author"] as! PFUser
+        let post = self.postsArray[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        cell.userLabel.text = user.username
-        cell.commentLabel.text = post["caption"] as? String
-        
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)!
-        cell.photoView.af.setImage(withURL: url)
-        
-        return cell
+        if indexPath.row == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PosterViewCell") as! PosterViewCell
+            
+            let user = post["author"] as! PFUser
+            
+            cell.userLabel.text = user.username
+            cell.commentLabel.text = post["caption"] as? String
+            
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)!
+            cell.photoView.af.setImage(withURL: url)
+            
+            return cell
+        } else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.postsArray.count
+        let post = postsArray[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return postsArray.count
     }
     
 
